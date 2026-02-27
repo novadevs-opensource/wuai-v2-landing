818c269ae9e6f4d1168879cdff347670edc0c75b
Bruno Lorente Cantarero <blorente@novadevs.com>
2026-02-27 11:10:57 +0100

    feat: add Lenis smooth scroll for inertia-based scrolling experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
 import IndexPage from './pages/IndexPage';
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.08,
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <>
       <IndexPage/>
